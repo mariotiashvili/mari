6cd6f91399a5117581a498d514f7b7791ade9d81
Gio <https://github.com/mariotiashvili/mari>
2022-11-29 23:01:19 +0400

    mari

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val yourname = findViewById<EditText>(R.id.yourname)
+        val password1 = findViewById<EditText>(R.id.password1)
+        val registrationbutton = findViewById<TextView>(R.id.registration)
+        val continuebutton = findViewById<TextView>(R.id.continuebutton)
+
+        intent = Intent(this,MainActivity3::class.java)
+        findViewById<TextView>(R.id.continuebutton).setOnClickListener {
+            var name = yourname.text.toString()
+            var pass = password1.text.toString()
+            if (name == "") {
+                Toast.makeText(this, "Please, Enter your Name!", Toast.LENGTH_SHORT).show()
+            } else if (pass == "") {
+                Toast.makeText(this, "Please, Enter your password!", Toast.LENGTH_SHORT).show()
+            } else {
+                startActivity(intent)
+
+            }
+
+
+        }
+        registrationbutton.setOnClickListener {
+            startActivity(Intent(this,MainActivity2::class.java))
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,46 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val name2 = findViewById<EditText>(R.id.name2)
+        val password2 = findViewById<EditText>(R.id.password2)
+        val password3 = findViewById<EditText>(R.id.password3)
+        val mynumber = findViewById<EditText>(R.id.mynumber)
+        val myprofilebutton = findViewById<TextView>(R.id.myprofilebutton)
+
+        intent = Intent(this,MainActivity3::class.java)
+        findViewById<TextView>(R.id.myprofilebutton).setOnClickListener {
+            var name3 = name2.text.toString()
+            var password4 = password2.text.toString()
+            var password5 = password3.text.toString()
+            var number1 = mynumber.text.toString()
+            if (name3 ==""){
+                Toast.makeText(this,"please,enter your name",Toast.LENGTH_LONG).show()
+            }
+            else if (password4 ==""){
+                Toast.makeText(this,"please,enter your password",Toast.LENGTH_LONG).show()
+            }
+            else if (password5 == ""){
+                Toast.makeText(this,"please,enter your password again",Toast.LENGTH_LONG).show()
+
+            }
+            else if (number1 ==""){
+                Toast.makeText(this,"please,enter your number",Toast.LENGTH_LONG).show()
+            }
+            else
+                startActivity(intent)
+
+        }
+
+    }
+}
